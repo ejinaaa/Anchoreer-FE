2800aebce8e882a9a90420e51062877f558f6ebf
ejinaaa <eunjink0423@gmail.com>
2024-11-12 23:13:33 +0900

    feat: install and setup react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import theme from './theme';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <ChakraProvider value={theme}>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider value={theme}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
