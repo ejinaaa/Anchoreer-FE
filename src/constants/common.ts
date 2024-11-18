cd782cea6dc84300c5b509cda1ccc56ab8fdffe7
ejinaaa <eunjink0423@gmail.com>
2024-11-18 22:01:19 +0900

    refactor: use constants for time unit

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 export const ONE_SECOND_IN_MS = 1000;
-export const ONE_MINUTE_IN_MS = 1000 * 60;
-export const ONE_HOUR_IN_MS = 1000 * 60 * 60;
+export const ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS * 60;
+export const ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * 60;
